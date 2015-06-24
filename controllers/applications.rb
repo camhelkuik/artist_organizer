c39c920d09057f11cdd127a1ba0b19b1d5671f6b
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-06-24 11:45:45 -0500

    Mispelling in db, needed to make new one. Working on views and controllers for the Applications Model

--- controllers/applications.rb
+++ controllers/applications.rb
@@ -2,3 +2,13 @@ get "/app_menu" do
   erb :"applications/app_menu"
 end
 
+get "/add_application" do
+  erb :"applications/add_app"
+end
+
+get "/save_application" do
+  @new_entry = Application.add({"events_id" => params["events_id"], "application_fee" => ["application_fee"], "event_fee" => ["event_fee"],
+   "submitted" => ["submitted"], "accepted" => ["accepted"], "due_date" => ["due_date"], "locations_id" => ["locations_id"], 
+   "application_details" => ["application_details"]})
+   erb :"app_success.erb"
+end
