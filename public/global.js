240c8a55e12a013c7acb8b850a0bd45bc265f74c
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-09 13:46:18 -0500

    Have the responsiveness working, except can not get the menu to display on larger devices. Still working on it

--- public/global.js
+++ public/global.js
@@ -11,3 +11,21 @@ hideMenu();
 document.getElementById("hamburger").onclick = showMenu;
 
 document.getElementById("hamburger").ondblclick = hideMenu;
+
+
+//
+//   var logo = document.getElementById("logo");
+//   var header = document.getElementById("header");
+//
+//   window.onscroll = shrink_header;
+//
+//   function shrink_header() {
+//     if (window.pageYOffset>120) {
+//     header.style.height = "75px";
+//     logo.className = "small"
+//   }
+//     if (window.pageYOffset<120) {
+//     header.style.height = "125px";
+//     logo.className = "large"
+//   }
+// }
